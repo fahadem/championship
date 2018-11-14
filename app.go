2fc35c1286b6d2083e1dcffef4e491688e4edb6d
Fahade <Vous@exemple.com>
2018-11-14 18:27:15 +0100

    First Commit

--- app.go
+++ app.go
@@ -16,7 +16,19 @@ import (
 //var config = Config{}
 var dao = LeaguesDAO{}
  
-func AllLeagueEndPoint(w http.ResponseWriter, r *http.Request) {
+func respondWithError(w http.ResponseWriter, code int, msg string) {
+	respondWithJson(w, code, map[string]string{"error": msg})
+}
+
+func respondWithJson(w http.ResponseWriter, code int, payload interface{}) {
+	response, _ := json.Marshal(payload)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(response)
+}
+
+
+func AllLeaguesEndPoint(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "not implemented yet !")
 }
  
