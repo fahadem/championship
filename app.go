de2a8bd45e334884ba85523aa75dbcad68d6b8a7
Fahade <Vous@exemple.com>
2018-11-18 14:15:36 +0100

    Update

--- app.go
+++ app.go
@@ -14,7 +14,7 @@ func champ(w http.ResponseWriter, r *http.Request) {
 func main() {
 	//in memory storage
 	leaguedb.Global_db = &leaguedb.LeaguesMongoDB{
-		DatabaseURL:           "mongodb://fahadem:Basket-ball11@ds051658.mlab.com:51658/championship",
+		DatabaseURL:           "mongodb://fahadem:'Basket-ball11'@ds051658.mlab.com:51658/championship",
 		DatabaseName:          "championship",
 		LeaguesCollectionName: "league",
 	}
@@ -24,7 +24,7 @@ func main() {
 
 	port := os.Getenv("PORT")
 
-	http.HandleFunc("/champ/", champ)
+	http.HandleFunc("/champ", champ)
 	http.HandleFunc("/champ/league", leaguedb.LeagueHandler)
 	http.ListenAndServe(":"+port, nil)
 }
